3c3d1aa8042348059ae59cf5240ff9c896e7c315
Krista LaFentres <lafentres@users.noreply.github.com>
2019-11-07 16:06:15 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+data "terraform_remote_state" "random-separator" {
+  backend = "remote"
+  config = {
+    organization = var.organization
+    workspaces = {
+      name = var.separator-workspace
+    }
+  }
+}
+
+data "terraform_remote_state" "random-length" {
+  backend = "remote"
+  config = {
+    organization = var.organization
+    workspaces = {
+      name = var.length-workspace
+    }
+  }
+}
+
+resource "random_pet" "pet" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  prefix    = "my"
+  length    = data.terraform_remote_state.random-length.outputs.integer
+  separator = data.terraform_remote_state.random-separator.outputs.separator
+}
